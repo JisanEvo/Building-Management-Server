37a125d8fb4ec56a3a9852f82924d14c4fa035e7
Jisan Evo~ <jesanmahmud6@gmail.com>
2024-06-23 13:30:21 +0600

    banner section done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId, Timestamp } = require('mongodb');
+
 const cors = require('cors');
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
@@ -8,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // middle wear
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['https://heaven-259e7.web.app'],
     credentials: true,
     optionSuccessStatus: 200,
 }
@@ -17,9 +19,9 @@ app.use(cors(corsOptions))
 app.use(express.json())
 //   app.use(cookieParser())
 
-console.log(process.env.DB_USER)
-const { MongoClient, ServerApiVersion, ObjectId, Timestamp } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qrw2ki7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// console.log(process.env.DB_USER)
+const uri = `mongodb+srv://${process.env.DB_SUER}:${process.env.DB_PASS}@cluster0.qrw2ki7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,7 +35,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const apartCollection = client.db("heavenDB").collection("apartment")
         const userCollection = client.db("heavenDB").collection("users")
         const cartCollection = client.db("heavenDB").collection("cart")
@@ -84,7 +86,7 @@ async function run() {
         // create-payment-intent
         app.post('/create-payment-intent', async (req, res) => {
             const price = req.body.price;
-            console.log(price)
+            // console.log(price)
              const priceInCent=parseFloat(price)*100;
             //  console.log(priceInCent)
            if(!price||priceInCent<1) return
@@ -180,7 +182,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
